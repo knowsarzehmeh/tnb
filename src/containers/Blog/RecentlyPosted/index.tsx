2b25b1be9730f1c7cc4ab498b7f0a47338fcbfc5
IvanDanilov66 <ivandanilov@rocketmail.com>
2021-03-05 00:30:06 +0100

    AddResponsivness

--- src/containers/Blog/RecentlyPosted/index.tsx
+++ src/containers/Blog/RecentlyPosted/index.tsx
@@ -1,11 +1,17 @@
 import React, {FC} from 'react';
 import Article from './Article';
-
+import Slider from './Slider';
 import './RecentlyPosted.scss';
 
 interface ArticlesProps {
   articles: any[];
 }
+const sliderProps = {
+  maxVisibleSlides: 3,
+  pageTransition: 250,
+  slideMargin: 15,
+  zoomFactor: 1,
+};
 
 const RecentlyPosted: FC<ArticlesProps> = ({articles}) => {
   return (
@@ -13,9 +19,12 @@ const RecentlyPosted: FC<ArticlesProps> = ({articles}) => {
       <div className="RecentlyPosted__content-container">
         <h3 className="RecentlyPosted__header">Recently Posted</h3>
         <div className="RecentlyPosted__article">
-          {articles.map((article) => (
-            <Article key={article} article={article} />
-          ))}
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Slider {...sliderProps}>
+            {articles.map((article) => (
+              <Article key={article} article={article} />
+            ))}
+          </Slider>
         </div>
       </div>
     </div>
