acf547f4b376bad697aa2338ded2480bf967d5b8
knowsarzehmeh <osarzeh@gmail.com>
2021-03-10 09:35:02 +0100

    Fetch Single Blog

--- src/containers/Blog/Article/index.tsx
+++ src/containers/Blog/Article/index.tsx
@@ -1,10 +1,12 @@
-import React, {FC, useEffect} from 'react';
-import {RouteChildrenProps} from 'react-router-dom';
+/* eslint-disable react/no-danger */
+import React, {FC, useMemo} from 'react';
+import {useParams} from 'react-router-dom';
 import {Icon, IconType, SocialMediaIcon} from 'components';
-import {Article as ArticleType} from 'types/blogs';
 import {SocialMedia} from 'types/social-media';
 
 import './Article.scss';
+import {slugify} from 'utils/urls';
+import {getArticleByTitle} from 'utils/data';
 
 // {
 //   id: number;
@@ -14,16 +16,15 @@ import './Article.scss';
 //   readTime: string;
 //   body: string;
 // };
+interface RouteParams {
+  slug: string;
+}
 
-// interface ComponentProps {
-//   article: ArticleType;
-// }
+const Article: FC = () => {
+  const {slug} = useParams<RouteParams>();
 
-const Article: FC<RouteChildrenProps> = (props) => {
-  useEffect(() => {
-    const {match} = props;
-    // console.log(match?.params.slug);
-  }, [props]);
+  const article = useMemo(() => getArticleByTitle(slugify(slug, '-', ' ')), [slug]);
+
   const renderSocialMediaLinks = () =>
     [SocialMedia.linkedin, SocialMedia.facebook, SocialMedia.twitter, SocialMedia.instagram].map((website) => (
       <SocialMediaIcon className="Article__SocialMediaLink" iconSize={28} key={website} website={website} />
@@ -35,32 +36,22 @@ const Article: FC<RouteChildrenProps> = (props) => {
         <div className="Article__wrapper">
           <div className="Article__content">
             <div className="Article__header">
-              <h1 className="Article__title">Cryptocurrency 101: Summary of the history and uses of cryptocurrency.</h1>
-              <p className="Article__user">by John Doe</p>
+              <h1 className="Article__title">{article?.title}</h1>
+              <div className="RecentlyPosted__author-wrapper">
+                <img
+                  className="RecentlyPosted__author-avatar"
+                  src={article?.author.avatar}
+                  alt={article?.author.name}
+                />
+                <p className="Article__user">by {article?.author.name}</p>
+              </div>
               <p className="Article__date">
-                Feb 17
-                <span className="Article__time">6 min read</span>
+                {article?.datePosted}
+                <span className="Article__time">{article?.readTime}</span>
               </p>
             </div>
             <div className="Article__banner" />
-            <div className="Article__body">
-              <p>
-                Generally, when the question: “what exactly is a cryptocurrency” comes up, it is easy to reply; “Digital
-                Currency”. Well, yes, but no. Let’s start at the top. Basically, cryptocurrency is the usage of digital
-                files as money/currency. The name is coined from two terms:
-              </p>
-              <p>
-                <span style={{fontWeight: 'bold'}}>Cryptoraphy</span>:The science of hiding information
-              </p>
-              <p>
-                <span style={{fontWeight: 'bold'}}>Currency</span>:Accepted system of money
-              </p>
-              <p>
-                Think you can properly define what a cryptocurrency actually is now? Exactly, “Digital Currency” is
-                easier.
-              </p>
-              <h3>History</h3>
-            </div>
+            <div className="Article__body" dangerouslySetInnerHTML={{__html: article?.content as string}} />
           </div>
         </div>
       </div>
