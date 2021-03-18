00f724179f3b01be509867b0822e56ec40ea3ec6
knowsarzehmeh <osarzeh@gmail.com>
2021-03-18 16:36:12 +0100

    Utils

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,4 +1,4 @@
 export const getFirstPathParam = (path: string): string => path.split('/').filter((pathParam) => !!pathParam)[0];
 
 export const slugify = (path: string, searchValue: string, replaceValue: string): string =>
-  path.replaceAll(searchValue, replaceValue);
+  path ? path.replaceAll(searchValue, replaceValue) : path;
